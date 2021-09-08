1bf0db3226e6f461cdf6d24a7881c1853caecdbb
Lukmanchehdo <Lukman.c@yru.ac.th>
2021-09-08 17:28:10 +0700

    commit ClassRoom

--- src/app/component/classroom/classroom.component.ts
+++ src/app/component/classroom/classroom.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ShareService } from '../../ShareService';
 
 @Component({
   selector: 'app-classroom',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassroomComponent implements OnInit {
 
-  constructor() { }
+  classs: any;
+  students: any;
+
+  classroom = {
+    id: null,
+    class_id : null,
+    student_id : null,
+  };
+
+  constructor(private http: HttpClient, private shareService: ShareService) { }
 
   ngOnInit(): void {
+    this.loadeClass();
+    this.loadeStudent();
   }
 
+  loadeClass() {
+    this.http.get(this.shareService.serverPath + '/class').subscribe((res: any) => {
+      this.classs = res.class
+      //console.log(this.classs);
+    })
+  }
+
+  loadeStudent() {
+    this.http.get(this.shareService.serverPath + '/student').subscribe((res: any) => {
+      this.students = res.student
+      // console.log(this.students);
+    });
+  }
+
+  saveClassroom() {
+    console.log(this.classroom);
+  }
 }
